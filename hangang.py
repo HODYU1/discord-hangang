4401c96e6472d026da2ce9b89e96cb6ac4bc473b
GithubNeon <87894558+GithubNeon@users.noreply.github.com>
2022-11-19 11:00:43 +0900

    Create hangang.py

---
+++ hangang.py
@@ -0,0 +1,13 @@
+@bot.slash_command(name="한강",description="서울 한강 수온을 알려줍니다")
+async def 한강(interaction:nextcord.Interaction):
+    await interaction.response.defer()
+    han=requests.get("https://api.hangang.msub.kr/").json()
+    temp=han["temp"]
+
+    with open("config/bot_option_list.json","r",encoding="UTF-8") as f:data=json.load(f)
+
+    wisenum=random.randint(1,50)
+
+    embed=nextcord.Embed(title=f"🏞 현재 한강 수온은 **{temp}℃** 입니다")
+    embed.set_footer(text=data["List"]["wise_saying"][str(wisenum)])
+    await interaction.followup.send(embed=embed)
